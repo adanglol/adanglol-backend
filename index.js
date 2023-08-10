fecc0934d701817c67481cb4ee8e266df37a74a3
Aaron Dangc <adraging@gmail.com>
2023-08-09 22:56:39 -0700

    test env

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const http = require('http');
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello, Node.js!');
+    res.end(process.env.EMAIL);
 });
 
 const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
