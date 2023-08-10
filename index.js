a2af884be42e9cd3221aa0eb2869162d4ad8cf38
Aaron Dangc <adraging@gmail.com>
2023-08-09 23:22:03 -0700

    added index html

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const express = require('express');
 const sgMail = require('@sendgrid/mail');
 const cors = require('cors');
 
-
 const PORT = process.env.PORT || 3000;
 const api_key = process.env.SENDGRID_API_KEY;
-
 sgMail.setApiKey(api_key);
+
 const app = express();
+
 app.use(cors());
-
-
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.get('/', (req, res) => {
     // console.log('GET request to /');
-    res.status(200).json({ message: 'GET endpoint' });
+    // res.status(200).json({ message: 'GET endpoint' });
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+
   });
 
 app.get('/send-email', (req, res) => {
