2ff3fde6d9600778c58aff90c29f3b4340e92f64
Aaron Dangc <adraging@gmail.com>
2023-08-09 23:03:47 -0700

    inital setup need implement email

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
+  
      // Use backticks (`) for template literals
      const emailMessage = `Email address from environment variable: ${process.env.EMAIL}`;
     
